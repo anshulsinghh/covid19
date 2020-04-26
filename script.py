c846c297696f159e22c9ff7181158764591e70f9
anshulsinghh <a2singh@ucsd.edu>
2020-04-26 06:25:09 +0000

    Added binomial model

--- script.py
+++ script.py
@@ -1,2 +1,9 @@
 import numpy as np
+
+n, p = 10, .5  # number of trials, probability of each trial
+s = np.random.binomial(n, p, 1000)
+# result of flipping a coin 10 times, tested 1000 times.
+
+print(s)
+
 print("Hello World")
